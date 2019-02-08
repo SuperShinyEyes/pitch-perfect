ff86adeca9ba84d875edc2a7e6276b8768654e56
Seyoung Park <seyoung.arts.park@protonmail.com>
2019-02-08 09:25:49 +0200

    Add YIN implementation as a Class

--- pitch_perfect/thinkdsp/asp.py
+++ pitch_perfect/thinkdsp/asp.py
@@ -89,3 +89,58 @@ def get_pitch_freq(ys):
         is_high_pitch = True if int(pitch[-1]) > 4 else False
     finally:
         return pitch, freq
+
+class YIN:
+
+    def __init__(self, ys, samplerate=44100):
+        self.ys = ys
+        self.samplerate = samplerate
+
+        self._diff = self.difference(self.ys)
+        self._cmn = self.cumulative_mean_normalized(self._diff)
+        self.freq = self.absolute_threshold(self._cmn, samplerate=samplerate)
+        self.pitch = freq2key(self.freq)
+    
+    def difference(self, ys):
+        N = len(ys)    
+        return np.array(
+            [ np.sum(
+                ( ys[lag:] - ys[:N-lag] ) ** 2
+                ) for lag in range(N//2) ]
+            )
+
+    
+    def cumulative_mean_normalized(self, diffs):
+        """
+        Args:
+            diffs (np.array): Difference over range of lags
+        """
+        cmn = np.zeros(diffs.shape)
+        cmn[0] = 1
+        for lag in range(1, len(diffs)) :
+            cmn[lag] = diffs[lag] / (
+            np.sum(diffs[1:lag+1]) / lag
+            )  
+        return cmn
+
+
+    def absolute_threshold(self, cmn, samplerate=44100):
+        """
+        Args:
+            cmn (np.array): Cumulative mean normalized difference
+        """
+        def cannot_apply_absolute_threshold(cmn):
+            return np.alltrue(cmn > 0.1)
+        
+        if cannot_apply_absolute_threshold(cmn):
+            # Return global minimum period
+            return  44100 / np.argmin(cmn)
+        
+        first_dip_start = np.argmax(cmn < 0.1)
+        first_dip_end = np.argmax(cmn[first_dip_start:] > 0.1) + first_dip_start
+
+        absolute_threshold_min = np.argmin(
+            cmn[first_dip_start:first_dip_end]
+        ) + first_dip_start
+
+        return 44100 / absolute_threshold_min
